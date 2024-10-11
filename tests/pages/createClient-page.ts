a29f8ecfc2166f5e45ba830046bfd26e43dcc694
Iftekhar Hossain <iftekhar.ctg30@gmail.com>
2024-10-11 18:15:16 +0200

    Final version

---
+++ tests/pages/createClient-page.ts
@@ -0,0 +1,37 @@
+import { expect, type Locator, type Page } from "@playwright/test";
+import { faker, Faker } from '@faker-js/faker';
+import { ClientsPage } from "./clients-page";
+
+export class CreateClientPage {
+  readonly page: Page;
+  readonly pageTitle: Locator;
+  readonly nameTextField: Locator;
+  readonly emailTextField:Locator;
+  readonly telephoneTextField:Locator;
+  readonly saveButtion: Locator;
+  readonly backButtion: Locator;
+
+
+  constructor(page: Page) {
+    this.page = page;
+    this.pageTitle = page.getByText('New Client');
+    this.nameTextField = page.locator('div').filter({ hasText: /^Name$/ }).getByRole('textbox');
+    this.emailTextField = page.locator('input[type="email"]');
+    this.telephoneTextField = page.locator('div').filter({ hasText: /^Telephone$/ }).getByRole('textbox');
+    this.saveButtion = page.getByText('Save');
+    this.backButtion = page.getByRole('link', { name: 'Back' }) ;
+    
+  }
+
+  async createClient(){
+    
+    const randomName = faker.person.fullName();
+    const randomEmail = faker.internet.email();
+    const randomTelephone = faker.phone.number();
+    await this.nameTextField.fill(randomName);
+    await this.emailTextField.fill(randomEmail);
+    await this.telephoneTextField.fill(randomTelephone);
+    await this.saveButtion.click();
+  }
+
+}
--- tests/testsuite.spec.ts
+++ tests/testsuite.spec.ts
@@ -1,12 +1,57 @@
 import { test, expect } from '@playwright/test';
 
-test.describe('Backend tests', ()=> {
-  test('Get all posts', async ({ request }) => {
-    const response = await request.get('http://localhost:3000/posts');
-    expect(response.ok()).toBeTruthy()
+test.describe('Front-end tests', () => {
+  test('log In and Create a Client', async ({ page }) => {
+    await page.goto('http://localhost:3000');
 
+
+    //Log in funtion
+    await page.locator('input[type="text"]').fill(`${process.env.TEST_USERNAME}`);
+    await page.locator('input[type="password"]').fill(`${process.env.TEST_PASSWORD}`);
+    await page.getByRole('button', { name: 'Login' }).click();
+
+    //Assertion
+    await expect(page.getByRole('heading', { name: 'Tester Hotel Overview' })).toBeVisible();
+    
+    await page.locator('#app > div > div > div:nth-child(2) > a').click();
+    await expect(page.getByRole('link', { name: 'Create Client' })).toBeVisible({ timeout: 10000 });
+    await page.getByRole('link', { name: 'Create Client' }).click();
+    await page.locator('div').filter({ hasText: /^Name$/ }).getByRole('textbox').click();
+    await page.locator('div').filter({ hasText: /^Name$/ }).getByRole('textbox').fill('iftekhar');
+    await page.locator('input[type="email"]').click();
+    await page.locator('input[type="email"]').fill('iftekhar@gmail.com');
+    await page.locator('div').filter({ hasText: /^Telephone$/ }).getByRole('textbox').click();
+    await page.locator('div').filter({ hasText: /^Telephone$/ }).getByRole('textbox').fill('0793131704');
+    await page.getByText('Save').click();
+    await page.waitForURL('http://localhost:3000/clients');
+    await expect(page.url()).toBe('http://localhost:3000/clients');
+    await expect(page.getByRole('link', { name: 'Tester Hotel' })).toBeVisible();
   });
+});
 
 
+test.describe('Backend tests', () => {
+  test('Log in and get all clients', async ({ request }) => {
+    const response = await request.post('http://localhost:3000/api/login', {
+      data:{
+        "username": `${process.env.TEST_USERNAME}`,
+        "password": `${process.env.TEST_PASSWORD}`
+      }      
+    });
+    expect (response.ok()).toBeTruthy();
+    const responseBody = await response.json();
+    const authToken = responseBody.token;
+    console.log(`Authentication token: ${authToken}`);
+    expect(authToken).toBeTruthy();
+    
+    console.log(`Authentication token: ${authToken}`);
+    const getClientsResponse = await request.get('http://localhost:3000/api/clients', {
+      headers: {
+        "content-type": "application/json",
+        'x-user-auth': `{"username":"tester01","token":"${authToken}"}`
+      }
+    });
+    expect(getClientsResponse.ok()).toBeTruthy();
 
-})  
+  });  
+});
